eaf27f0a7a812216669953535ddcdadf2963c9ff
fusioneventsak <arthurk@fusion-events.ca>
2025-07-16 17:40:44 -0400

    Remove Blog link from header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -39,16 +39,6 @@ const Header: React.FC = () => {
               </Link>
             </div>
             
-            <Link
-              to="/blog"
-              className="px-3 py-2 rounded-md text-sm font-medium text-gray-200 hover:bg-purple-500/20 hover:text-white transition-colors flex items-center"
-            >
-              <svg className="h-4 w-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
-              </svg>
-              Blog
-            </Link>
-            
             {/* Mobile menu button */}
             <div className="flex items-center space-x-3">
               <Link
